ce6cec4c8673f1ec41be0be242cfd6f159e4b94c
Michał Kasprzyk <kasprzyk.michal@me.com>
2018-06-08 09:26:02 -0700

    Initial Commit

---
+++ BotTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BotTest
+//
+//  Created by Michal Kasprzyk on 08/06/2018.
+//  Copyright © 2018 Michal Kasprzyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
